c3e4635ad23657b34e11dc7bbe06e63e8713b5fe
Marcko <marco.ramireze@gmail.com>
2024-03-24 17:57:26 -0600

    Add sonarAnalysis

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,15 @@
+def call(){
+    
+    def scannerHome = tool 'sonar-scanner'
+    
+    withSonarQubeEnv(credentialsId: 'sonar-token-new'){
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=sonarqube \
+            -Dsonar.sources=. \
+            -Dsonar.host.url=http://localhost:9000 \
+            -Dsonar.token=sqa_ef6b785d0c53d831a09f0670e3bc76c390659a3e
+        """
+    }
+
+}
