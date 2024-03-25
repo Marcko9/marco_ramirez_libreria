cc40392143752d568048bfc49fd128fc542782b6
Marcko <marco.ramireze@gmail.com>
2024-03-24 18:09:42 -0600

    Add boolean var

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(boolean abortPipeline){
     
     def scannerHome = tool 'sonar-scanner'
     
@@ -10,6 +10,14 @@ def call(){
             -Dsonar.host.url=http://localhost:9000 \
             -Dsonar.token=sqa_ef6b785d0c53d831a09f0670e3bc76c390659a3e
         """
+    }
+
+    timeout(time: 1, unit: 'MINUTES') {
+        if (abortPipeline) {
+            error 'Marcko - Abort pipeline.'       
+        } else {
+            echo 'Marcko - Pipeline OK'
+        }
     }
 
 }
